e2905fafb4d613b2e61a21ded0234260703f1d00
Martin Ortega <lynxestudio@outlook.com>
2023-11-09 17:54:56 -0600

    Modify add HTML Tags to the file.

---
+++ src/Delegates/Temp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Samples.Delegates
+{
+	public class Temp
+	{
+		//la implementación de cada metódo
+		public static double GetCelsius(double fahrenheit)
+		{
+			return (fahrenheit - 32) * (5 / 9D);
+		}
+
+		public static double GetKelvin(double fahrenheit)
+		{
+			return fahrenheit + 460;
+		}
+	}
+}
